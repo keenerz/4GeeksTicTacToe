6427a191eb10c690fca4dad8dc8eac675a8d17fb
Keenan <kkan68@gmail.com>
2021-12-14 06:32:30 +0000

    js work needed

--- src/app.js
+++ src/app.js
@@ -4,8 +4,36 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let board = [
+  null,
+  "fas fa-times",
+  null,
+  null,
+  null,
+  "far fa-circle",
+  null,
+  "far fa-circle",
+  null
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".board").innerHTML = board
+    .map(elm => drawGrid(elm))
+    .join("");
 };
+
+function drawGrid(symbol = "fas fa-times") {
+  return `<div class="grid">
+  <i class="${symbol}"></i>
+</div>`;
+}
+
+let addin = document.querySelector(".grid");
+if (addin) {
+  addin.addEventListener("click", function(e) {
+    board[e.target] = "fas fa-times";
+    console.log(e.target);
+  });
+}
